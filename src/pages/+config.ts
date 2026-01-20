d3a9e6960792d5854da244a6c5974bc1cdb38a96
Mel <romelcub@gmail.com>
2026-01-20 18:08:44 +0800

    Updates : Tue, Jan 20, 2026  6:08:43 PM

--- src/pages/+config.ts
+++ src/pages/+config.ts
@@ -1,6 +1,7 @@
-import vikeReact from 'vike-react/config'
+import vikeReact from 'vike-react/config';
+import vikePhoton from 'vike-photon/config';
 export default {
   title: 'Romel Cubelo | Web Developer Portfolio',
   description: 'Portfolio of Romel Cubelo, web developer specializing in React, PHP, and Laravel.',
-  extends: [vikeReact]
+  extends: [vikeReact,vikePhoton]
 }
